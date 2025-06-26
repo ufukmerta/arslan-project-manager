8cc252d0222d13960692625db736eb87f56f40e4
Ufuk Mert Arslan <ufukmertarslan@gmail.com>
2025-06-26 23:38:36 +0300

    Add WebUI Project

---
+++ ArslanProjectManager.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArslanProjectManager.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
