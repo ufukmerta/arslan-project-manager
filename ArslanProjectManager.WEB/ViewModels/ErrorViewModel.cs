001abf43596f3920ce4b2e6bcc1cec53f5acf045
Ufuk Mert Arslan <ufukmertarslan@gmail.com>
2025-06-26 20:21:58 +0300

    Remove UI and WEB projects to implement later

--- ArslanProjectManager.WEB/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ArslanProjectManager.WEB.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
