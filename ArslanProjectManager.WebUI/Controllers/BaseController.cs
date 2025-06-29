51f41cefd73d70ffd503fc9483606284006643cf
Ufuk Mert Arslan <ufukmertarslan@gmail.com>
2025-06-29 03:28:58 +0300

    Add base controller for shared response handling
    Created a base controller that provides shared JSON deserialization options and a method for extracting error messages from HTTP responses.

---
+++ ArslanProjectManager.WebUI/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using ArslanProjectManager.Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace ArslanProjectManager.WebUI.Controllers
+{
+    public class BaseController : Controller
+    {
+        private protected readonly JsonSerializerOptions _jsonSerializerOptions = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        private protected async Task<string?> GetErrorMessageAsync(HttpResponseMessage response)
+        {
+            var jsonError = await response.Content.ReadAsStreamAsync();
+            var errorResponse = await JsonSerializer.DeserializeAsync<CustomResponseDto<NoContentDto>>(jsonError, _jsonSerializerOptions);
+            return errorResponse?.Errors?.FirstOrDefault() ?? "An error occurred while processing your request. Please try again later.";
+            
+        }
+    }
+}
