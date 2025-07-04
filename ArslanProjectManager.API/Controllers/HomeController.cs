aa5421f7cdfd237b137adf37ebe288c9f8a227d4
Ufuk Mert Arslan <ufukmertarslan@gmail.com>
2025-07-04 14:06:49 +0300

    Refactor HomeController to remove unused dependencies and namespaces
    - Removed ProjectManagerDbContext and ITokenService from constructor
    - Improves code clarity and reduces unnecessary dependency injection and using keyword for namespaces

--- ArslanProjectManager.API/Controllers/HomeController.cs
+++ ArslanProjectManager.API/Controllers/HomeController.cs
@@ -1,24 +1,16 @@
-﻿using ArslanProjectManager.Core;
-using ArslanProjectManager.Core.DTOs;
+﻿using ArslanProjectManager.Core.DTOs;
 using ArslanProjectManager.Core.Models;
 using ArslanProjectManager.Core.Services;
-using ArslanProjectManager.Core.ViewModels;
-using ArslanProjectManager.Repository;
-using ArslanProjectManager.Service.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 
 namespace ArslanProjectManager.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class HomeController(ProjectManagerDbContext context, IHomeService homeService, IUserService userService, ITokenService tokenService) : CustomBaseController(tokenService)
+    public class HomeController(IHomeService homeService, IUserService userService, ITokenService tokenService) : CustomBaseController(tokenService)
     {
-        private readonly ProjectManagerDbContext _context = context;
         private readonly IHomeService _homeService = homeService;
         private readonly IUserService _userService = userService;
-        private readonly ITokenService _tokenService = tokenService;
 
         [HttpGet("")]
         public async Task<IActionResult> IndexAsync()
