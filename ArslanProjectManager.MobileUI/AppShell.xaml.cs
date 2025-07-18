a15c79db59e494e0ef97fd29c2d21766231f58cb
Ufuk Mert Arslan <ufukmertarslan@gmail.com>
2025-07-18 15:44:10 +0300

    Add .NET MAUI project

---
+++ ArslanProjectManager.MobileUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ArslanProjectManager.MobileUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
